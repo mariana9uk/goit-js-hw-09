b5b69e74a4b0216f7d0220055577cb85710e2b4a
mariana9uk <maryanagolyuk@gmail.com>
2023-07-04 20:27:11 +0300

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
-
+const startBtn = document.querySelector("button[data-start]");
+console.log(startBtn);
+const stopBtn = document.querySelector("button[data-stop]");
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+  let timerId;
+  stopBtn.disabled=true;
+  startBtn.addEventListener('click', ()=>{
+    if (!timerId) {
+        timerId=setInterval(()=>{document.body.style.backgroundColor=getRandomHexColor()}, 1000);
+        startBtn.disabled=true;
+        stopBtn.disabled=false;
+    }
+    });
+    stopBtn.addEventListener('click', ()=>{
+   if (timerId) {
+    clearInterval(timerId);
+    timerId=null;
+            startBtn.disabled=false;
+            stopBtn.disabled=true;
+   }
+            })
+    
